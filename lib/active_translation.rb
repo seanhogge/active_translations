2d882a705c5e70c6da4a22778e53502bf0010731
Sean Hogge <sean@seanhogge.com>
2025-09-25 09:42:29 -0500

    fix autoload/require issues

--- lib/active_translation.rb
+++ lib/active_translation.rb
@@ -3,6 +3,9 @@ require "active_translation/engine"
 require "active_translation/configuration"
 require "active_translation/translatable"
 require "active_translation/translation_job"
+require "faraday"
+require "faraday_middleware"
+require "googleauth"
 
 module ActiveTranslation
   class << self
